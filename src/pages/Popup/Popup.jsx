78a2cbdb599c562cea9d9501879a263035f14db7
saleemunqayoom <saleem.uq@accubits.com>
2023-01-07 11:54:46 +0530

    setup

---
+++ src/pages/Popup/Popup.jsx
@@ -0,0 +1,20 @@
+import './Popup.css';
+
+import Greetings from '../../containers/Greetings/Greetings';
+import React from 'react';
+import logo from '../../assets/img/logo.svg';
+
+const Popup = () => {
+  return (
+    <div className="App">
+      <h1 className="text-6xl bg-red-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        minima excepturi culpa nesciunt adipisci velit amet debitis? Esse illo
+        ullam necessitatibus hic! Consequuntur vel magnam repellat animi cum
+        neque iusto?
+      </h1>
+    </div>
+  );
+};
+
+export default Popup;
